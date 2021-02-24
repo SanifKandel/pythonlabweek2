7802cd53fcf25c12a63c07c2f5d9d879e760bbfd
Sanif <sanifkandel12345@gmail.com>
2021-02-24 12:06:19 +0545

    Distribute Apple

---
+++ Lab1/Apple.py
@@ -0,0 +1,11 @@
+# WAP N students take K apple and distribute them along each other evenly. The remaining parts
+# remain in the basket. How many apples will each single student get ? How many apples will remain in the basket ? The
+#program reads the number N and K . It should print the two answers to the above questions.
+
+"""Std1 =int(input('No. of Student :'))
+app1 = int(input('Total no. of apples :'))
+Dist =  int(app1/Std1)
+print ('Each Student will get', Dist)
+Rem = app1 - Dist*Std1
+print ('The remaining apple is', Rem)
+"""
