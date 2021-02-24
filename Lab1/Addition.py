d5f1d7695e41a425a92ae23d10be48e276ccc6aa
Sanif <sanifkandel12345@gmail.com>
2021-02-24 11:16:49 +0545

    Initial commit

---
+++ Lab1/Addition.py
@@ -0,0 +1,7 @@
+#Write a program that take three numbers and print their sum . Every number is given on a separate line.
+
+val1 = float(input('First number is: \n'))
+val2 = float(input('First number is: \n'))
+val3 = float(input('First number is: \n'))
+sum= val1 + val2 + val3
+print (sum)
