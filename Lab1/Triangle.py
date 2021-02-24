07b3830fa41b1f3eaf063c80580a947922f7c55e
Sanif <sanifkandel12345@gmail.com>
2021-02-24 11:24:44 +0545

    Area of traingle

---
+++ Lab1/Triangle.py
@@ -0,0 +1,6 @@
+#Write a program that reads the length of the base and the height of a right-angled triangle and print the area . Every number is given on a separate line.
+
+len1 =float(input('Enter the length of base: \n'))
+hgt1 =float(input('Enter the height: \n'))
+area = 1/2 * (len1 * hgt1)
+print (area)
